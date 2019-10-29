0e664b8cd9a152d588a214211459dfaf7219baae
Joe Billingsley <jb@josephbillingsley.co.uk>
2019-10-29 17:22:29 +0000

    More data, fixed graphs

--- batch_script.sh
+++ batch_script.sh
@@ -2,7 +2,7 @@
 cd /media/joebillingsley/Data/projects/NFV_FatTree/simulations
 
 # If it's stupid and it works... 
-# ../src/Simulations -m -u Cmdenv -c LowNumPorts -n .:../src --cpu-time-limit=20s omnetpp.ini
+../src/Simulations -m -u Cmdenv -c LowNumPorts -n .:../src --cpu-time-limit=20s omnetpp.ini
 ../src/Simulations -m -u Cmdenv -c IncreasingNumPorts -n .:../src --cpu-time-limit=20s omnetpp.ini
 # ../src/Simulations -m -u Cmdenv -c IncreasingSDN -n .:../src --cpu-time-limit=20s omnetpp.ini
 # ../src/Simulations -m -u Cmdenv -c FilteringVNFs -n .:../src --cpu-time-limit=20s omnetpp.ini
